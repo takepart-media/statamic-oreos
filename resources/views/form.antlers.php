5ade57df2715088e359885dcb8c269f8bae5f68c
Jack McDade <jack@jackmcdade.com>
2022-03-21 16:20:01 -0400

    Use native tags instead of PHP

--- resources/views/form.antlers.php
+++ resources/views/form.antlers.php
@@ -1,57 +1,50 @@
 <form action="{{ route:statamic.oreos.save }}" method="post">
-    <input type="hidden" name="_token" value="<?php echo csrf_token() ?>">
+    <input type="hidden" name="_token" value="{{ csrf_token }}">
 
     <div class="flex flex-col gap-2 my-8">
         {{ oreos }}
-            <div>
-                <label class="flex items-baseline">
-                    <input
-                        type="checkbox"
-                        name="oreos[]"
-                        value="{{ handle }}"
-                        {{ checked ? 'checked' : '' }}
-                        {{ required ? 'required disabled' : '' }}
-                        class="mr-2 {{ required ? ' opacity-40' : '' }}"
-                    >
-                    <div>
-                        <p class="font-semibold">{{ title }}</p>
+        <div>
+            <label class="flex items-baseline">
+                <input type="checkbox" name="oreos[]" value="{{ handle }}" {{ checked ? 'checked' : '' }} {{ required ? 'required disabled' : '' }} class="mr-2 {{ required ? ' opacity-40' : '' }}">
+                <div>
+                    <p class="font-semibold">{{ title }}</p>
 
-                        {{ if showDescription && description }}
-                            <p class="text-sm text-gray-600">{{ description }}</p>
-                        {{ /if }}
-                    </div>
-                </label>
-            </div>
+                    {{ if showDescription && description }}
+                    <p class="text-sm text-gray-600">{{ description }}</p>
+                    {{ /if }}
+                </div>
+            </label>
+        </div>
         {{ /oreos }}
     </div>
 
     <div class="flex gap-2">
         <button class="py-1 px-2 bg-gray-200 border hover:border-current" type="submit" name="action" value="save">
-            <?php echo __('statamic-oreos::messages.button.save') ?>
+            {{ trans key="statamic-oreos::messages.button.save" }}
         </button>
 
         {{ if showAcceptall }}
-            <button class="py-1 px-2 border hover:border-current" type="submit" name="action" value="accept-all">
-                <?php echo __('statamic-oreos::messages.button.acceptall') ?>
-            </button>
+        <button class="py-1 px-2 border hover:border-current" type="submit" name="action" value="accept-all">
+            {{ trans key="statamic-oreos::messages.button.acceptall" }}
+        </button>
         {{ /if }}
 
         {{ if showCancel }}
-            <button class="py-1 px-2 border hover:border-current" type="reset" onclick="removeOreosPopup()">
-                <?php echo __('statamic-oreos::messages.button.cancel') ?>
-            </button>
-            <script>
-                function removeOreosPopup() {
-                    const el = document.getElementById('{{ popupId ?? "oreos-popup" }}');
-                    if (el) el.parentNode.removeChild(el);
-                }
-            </script>
+        <button class="py-1 px-2 border hover:border-current" type="reset" onclick="removeOreosPopup()">
+            {{ trans key="statamic-oreos::messages.button.cancel" }}
+        </button>
+        <script>
+            function removeOreosPopup() {
+                const el = document.getElementById('{{ popupId ?? "oreos-popup" }}');
+                if (el) el.parentNode.removeChild(el);
+            }
+        </script>
         {{ /if }}
 
         {{ if showReset }}
-            <button class="py-1 px-2 border hover:border-current" type="submit" name="action" value="reset">
-                <?php echo __('statamic-oreos::messages.button.reset') ?>
-            </button>
+        <button class="py-1 px-2 border hover:border-current" type="submit" name="action" value="reset">
+            {{ trans key="statamic-oreos::messages.button.reset" }}
+        </button>
         {{ /if }}
 
     </div>
